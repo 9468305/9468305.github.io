eb2c6bbcb562cd887478dd3ba534bbcc8355a617
ChenQi <9468305@gmail.com>
2020-07-19 01:44:15 +0800

    Verify mermaid config useMaxWidth true

--- assets/js/mermaid.init.js
+++ assets/js/mermaid.init.js
@@ -1,10 +1,6 @@
 var config = {
   startOnLoad:true,
   theme: "forest",
-  flowchart:{
-    useMaxWidth:false,
-    htmlLabels:true
-  }
 };
 mermaid.initialize(config);
 window.mermaid.init(undefined, document.querySelectorAll('.language-mermaid'));
